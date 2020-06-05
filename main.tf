efdc95e962205cc49fa56ea1e9927d31a8b55bbf
Kevin Schoonover <schoonoverkevinm@gmail.com>
2020-06-05 00:10:09 -0500

    Add all latest files

--- main.tf
+++ main.tf
@@ -2,12 +2,31 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-resource "digitalocean_droplet" "default" {
-  name = "test-droplet"
+resource "digitalocean_droplet" "default-project" {
+  name = "droplet-with-project"
   image = "ubuntu-18-04-x64"
   region = "nyc3"
   size = "s-1vcpu-1gb"
   monitoring = true
   private_networking = true
-  tags = ["test_droplet", "test"]
+  tags = ["env_prod", "project_test"]
+}
+
+resource "digitalocean_droplet" "default-noproject" {
+  name = "droplet-without-project"
+  image = "ubuntu-18-04-x64"
+  region = "nyc3"
+  size = "s-1vcpu-1gb"
+  monitoring = true
+  private_networking = true
+  tags = ["env_prod", "project_test"]
+}
+
+
+resource "digitalocean_project" "default" {
+  name = "test"
+  description = "test project"
+  purpose = "Other"
+  environment = "Development"
+  resources   = [digitalocean_droplet.default-project.urn]
 }
